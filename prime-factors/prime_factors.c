6da4e1c591b4b5e69aa8a259ee97cc48bdff49c6
BrainGithub <116586276@qq.com>
2016-12-09 06:01:33 -0800

    add recursive methos of getting prime factors

--- prime-factors/prime_factors.c
+++ prime-factors/prime_factors.c
@@ -6,42 +6,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-/*
-int get_prime_factors(int data, int * factors)
-{
-    int n;
-    int candidate = 2;
-
-    for (n=0; candidate <= data; candidate++){
-        for (; data % candidate == 0; data /= candidate){
-            factors[n++] = candidate;
-        }
-    }
-
-    return n;
-}
-
-*/
-int get_prime_factors_recursive(int data, int * factors, int count, int candidate);
 
 int get_prime_factors(int data, int * factors)
 {
-        return get_prime_factors_recursive(data, factors, 0, 2);
-}
+	int n = 0;
+	int candidate = 2;
 
-int get_prime_factors_recursive(int data, int * factors, int count, int candidate)
-{
-    if (data < 2)
-        return count;
+	for (; data >= candidate; candidate++)
+	{
+		for (; data % candidate == 0; data /= candidate)
+		{
+			factors[n++] = candidate;
+		}
+	}
 
-    if (data % candidate == 0){
-        factors[count++] = candidate;
-        data /= candidate;
-    }
-    else
-    {
-        candidate++;
-    }
-
-    return get_prime_factors_recursive(data, factors, count, candidate);
+	return n;
 }

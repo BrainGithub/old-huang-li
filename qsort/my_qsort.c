0d0e6d80fa961cd6eb818429371e2529ce55cfbf
BrainGithub <116586276@qq.com>
2016-12-09 20:21:22 -0800

    impliment my qsort

--- qsort/my_qsort.c
+++ qsort/my_qsort.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int my_qsort()
+int my_qsort(int * arr, int const left, int const right)
 {
+	int n;
+	int i=left,j=right;
+	int pivot = arr[i];
+	
+	if (left >= right)
+		return;
+
+	for(n=left; n<=right; n++)
+		printf(" %d",arr[n]);
+	printf("--before sort\n");
+	
+	while(i < j){
+		for (;i<j && arr[j] > pivot; j--);
+		if (i<j) arr[i++] = arr[j];
+		for (;i<j && arr[i] < pivot; i++);
+		if (i<j) arr[j] = arr[i];
+	}
+	arr[i] = pivot;
+	printf("i:%d,j:%d\n",i,j);
+	for(n=left; n<=right; n++)
+		printf(" %d",arr[n]);
+	printf("--after sort\n");
+
+	sleep(1);
+	my_qsort(arr, left, i-1);
+	my_qsort(arr, i+1, right);
 	return 0;
 }
--- qsort/test.c
+++ qsort/test.c
@@ -4,9 +4,7 @@
 
 static int cmp(const void *p1, const void *p2, void * priv)
 {
-/* The actual arguments to this function are "pointers to
-*pointers to char", but strcmp(3) arguments are "pointers
-*to char", hence the following cast plus dereference */
+	//return: < -1, == 0, > 1
 	return *(int *)p1 < *(int *)p2 ? -1:(*(int *)p1 == *(int *)p2 ? 0:1);
 }
 
@@ -14,6 +12,7 @@ int main(int argc, char *argv[])
 {
 	int j;
 	int arr[] = {1, -1, 5, 3};
+	int arr2[] = {1, -1, 5, 3};
 		            
 	if (argc < 2) {
 		fprintf(stderr, "Usage: %s <string>...\n", argv[0]);
@@ -22,10 +21,15 @@ int main(int argc, char *argv[])
 
 
 	qsort_r(arr, 4, sizeof(int), cmp, NULL);
+	my_qsort(arr2, 0, 3); 
 
 	for (j = 0; j < 4; j++)
 		printf(" %d", arr[j]);
 	printf("\n");
-	exit(EXIT_SUCCESS);
+
+	if (memcmp(arr, arr2, sizeof(int) *4))
+		printf("---failed\n");
+
+	return 0;	
 }
 
